aca8bad87a4c58fdd5d2e57eb4251829a3dcc6c8
IraMira333 <irinat333@gmail.com>
2023-11-16 16:32:15 +0200

    Create pages Catalog, Favorite, Home

---
+++ src/pages/CatalogPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CatalogPage() {
+  return <h2>Catalog Cars</h2>;
+}
---
+++ src/pages/FavoritePage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FavoritePage() {
+  return <h2>Favorite Cars</h2>;
+}
