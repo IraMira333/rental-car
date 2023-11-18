59c4b7fa76b4fdbdd16ae802047ad54c4d3df5d7
IraMira333 <irinat333@gmail.com>
2023-11-18 23:26:43 +0200

    Create API.js

---
+++ src/services/API.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+axios.defaults.baseURL =
+  '<https://64ca4e55700d50e3c704af47.mockapi.io/contacts>';
+
+export const getCars = async ({ limit = 12, ...rest }) => {
+  const response = await axios.get('', { params: { limit, ...rest } });
+  console.log(response.data);
+  return response.data;
+};
