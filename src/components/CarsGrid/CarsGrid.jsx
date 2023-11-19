b64ef2da2c2c68ef213a5f0ee08d3384362be1ba
IraMira333 <irinat333@gmail.com>
2023-11-20 01:08:16 +0200

    Add ul

--- src/components/CarsGrid/CarsGrid.jsx
+++ src/components/CarsGrid/CarsGrid.jsx
@@ -2,13 +2,13 @@ import CarCard from 'components/CarCard/CarCard';
 
 const CarsGrid = ({ cars }) => {
   return (
-    <>
+    <ul>
       {cars.map(car => (
         <li key={car.id}>
           <CarCard car={car} />
         </li>
       ))}
-    </>
+    </ul>
   );
 };
 
