7ba856547c71dcb7ead6939113f3e4a31e50913e
IraMira333 <irinat333@gmail.com>
2023-11-20 01:07:50 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
 export const selectFavoriteCars = state => state.favoriteCars;
 export const selectIsLoading = state => state.isLoading;
 export const selectIsLastPage = state => state.isLastPage;
+export const selectCars = state => state.carsInfo;
