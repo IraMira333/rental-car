94f744f976d0cfd650c0e2d5ee0112049f34c24d
IraMira333 <irinat333@gmail.com>
2023-11-21 01:04:34 +0200

    Deleted pagination params so far

--- src/services/API.js
+++ src/services/API.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  '<https://64ca4e55700d50e3c704af47.mockapi.io/contacts>';
+axios.defaults.baseURL = `https://64ca4e55700d50e3c704af47.mockapi.io/contacts/`;
 
-export const getCarsAPI = async ({ limit = 12, ...rest }) => {
-  const response = await axios.get('', { params: { limit, ...rest } });
-  console.log(response.data);
+export const getCarsAPI = async () => {
+  const response = await axios.get('/cars');
+
   return response.data;
 };
