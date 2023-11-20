981688739d0f7be547b5c88939e8ad5a58a65a0f
IraMira333 <irinat333@gmail.com>
2023-11-21 01:03:00 +0200

    Сleaned from unused variables

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,16 +1,15 @@
 import CarsGrid from 'components/CarsGrid/CarsGrid';
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCars } from 'redux/operations';
-import { selectCars, selectIsLastPage, selectIsLoading } from 'redux/selectors';
+import { selectCars } from 'redux/selectors';
 
 export default function CatalogPage() {
   const dispatch = useDispatch();
   const cars = useSelector(selectCars);
-  const isLastPage = useSelector(selectIsLastPage);
-  const isLoading = useSelector(selectIsLoading);
-  const [params, setParams] = useState({ page: 1 });
+  // const isLastPage = useSelector(selectIsLastPage);
+  // const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(getCars());
