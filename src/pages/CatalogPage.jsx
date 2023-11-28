6e5466930d8c8db3db7a4c8d96f78dea7576e26e
IraMira333 <irinat333@gmail.com>
2023-11-28 17:47:00 +0200

    Update CatalogPage.jsx

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,9 +1,8 @@
-import CarsGrid from 'components/CarsGrid/CarsGrid';
-import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCars } from 'redux/operations';
 import { selectCars } from 'redux/selectors';
+import CarsGrid from 'components/CarsGrid/CarsGrid';
 
 export default function CatalogPage() {
   const dispatch = useDispatch();
