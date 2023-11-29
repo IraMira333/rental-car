47181d396694fc88e72881522e8a1f6f5ac893b5
IraMira333 <irinat333@gmail.com>
2023-11-29 14:32:23 +0200

    Created and Styled LoadMoreButton

---
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -0,0 +1,7 @@
+import { LoadMoreStyle } from './LoadMoreButton.styled';
+
+const LoadMoreButton = props => {
+  return <LoadMoreStyle {...props}>Load More</LoadMoreStyle>;
+};
+
+export default LoadMoreButton;
---
+++ src/components/LoadMoreButton/LoadMoreButton.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const LoadMoreStyle = styled.button`
+  align-self: center;
+
+  font-family: Manrope;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.5; /* 150% */
+  text-decoration-line: underline;
+  color: #3470ff;
+  border: none;
+  background-color: transparent;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    color: #0b44cd;
+  }
+`;
