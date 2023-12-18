2de0f5e8079461e67144b38ac638d5e69abc4528
IraMira333 <irinat333@gmail.com>
2023-12-18 20:12:33 +0200

    Change Logo & Favicon

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import { StyledHeader, StyledNavLink } from './AppBar.styled';
+import carLogo from '../../assets/carRentCut.png';
 
 export default function AppBar() {
   return (
     <>
       <StyledHeader>
+        <NavLink to="/">
+          <img src={carLogo} alt="logo" height="60" />
+        </NavLink>
         <StyledNavLink to="/">Home</StyledNavLink>
         <StyledNavLink to="/catalog">Catalog</StyledNavLink>
         <StyledNavLink to="/favorites">Favorites</StyledNavLink>
